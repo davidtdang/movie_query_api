68041969ca44f3486add3bc49e50ec4ab47528a3
David Dang <dtudang@gmail.com>
2015-01-26 17:07:22 -0800

    removed ID

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,11 +11,8 @@ class MoviesController < ApplicationController
   def search
     if params.has_key?("t")
       @movie = Movie.select{|movie| movie.title.downcase.include?(params["t"].downcase)}
-      # @movie = Movie.select(:title => params[:t])
     elsif params.has_key?("y")
       @movie = Movie.select{|movie| movie.year == params["y"].to_i}
-    elsif params.has_key?("i")
-      @movie = Movie.select{|movie| movie.id == params["i"].to_i}
     end
     render json: @movie
   end
